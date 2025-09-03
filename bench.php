112670af3a1ec2a1486d3518304fec438a4e5b2f
Jon Surrell <sirreal@users.noreply.github.com>
2025-09-03 13:47:01 +0200

    Make PHP benchmarking code

--- bench.php
+++ bench.php
@@ -1,100 +1,115 @@
+#!/opt/homebrew/opt/php@8.4/bin/php -d opcache.enable_cli=true -d opcache.jit=on -d opcache.jit_buffer_size=50M
 <?php
-$db = new mysqli(
-	'127.0.0.1',
-	'test_user',
-	'test_pass',
-	'test_db',
-	6330
-);
+ini_set( 'strict_types', '1' );
 
-if ( $db->connect_error ) {
-	echo "Connection error: " . $db->connect_error . "\n";
-	exit( 1 );
+const ITERATIONS = 100_000;
+
+function test1_setup($db) {
+	echo "\n***** Test: 1 *****\n";
+	echo "> NO TSA index\n";
+	echo "\n";
+	run_sql_file( $db, 'create-table.sql' );
+	run_sql_file( $db, 'autoinc.sql' );
+	run_sql_file( $db, 'wp-posts-data.sql' );
+}
+function test1($db) {
+	run_explain_count( $db );
 }
 
-$db->query( "SET SESSION sql_mode = REPLACE(@@sql_mode, 'NO_ZERO_DATE', '')" );
-$db->query( "SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION'" );
+function test2_setup($db) {
+	echo "\n***** Test: 2 *****\n";
+	echo "> New TSA index immediately after create table, before insert\n";
+	echo "\n";
+	run_sql_file( $db, 'create-table.sql' );
+	run_sql_file( $db, 'autoinc.sql' );
+	run_sql_file( $db, 'tsa-index.sql' );
+	run_sql_file( $db, 'wp-posts-data.sql' );
+}
+function test2($db) {
+	run_explain_count( $db,  );
+}
 
-//
+function test2b_setup($db) {
+	echo "\n***** Test: 2b *****\n";
+	echo "> New TSA index immediately after create table, before insert (alt data set)\n";
+	echo "\n";
+	run_sql_file( $db, 'create-table.sql' );
+	run_sql_file( $db, 'autoinc.sql' );
+	run_sql_file( $db, 'tsa-index.sql' );
+	run_sql_file_each_line( $db, 'wp-posts-data-alt.sql' );
+}
+function test2b($db) {
+	run_explain_count( $db );
+}
 
-echo "\n***** Test: 1 *****\n";
-echo "> NO TSA index\n";
-echo "\n";
+function test3_setup($db) {
+	echo "\n***** Test: 3 *****\n";
+	echo "> New TSA index immediately after create table, then analyze, before insert\n";
+	echo "\n";
+	run_sql_file( $db, 'create-table.sql' );
+	run_sql_file( $db, 'autoinc.sql' );
+	run_sql_file( $db, 'tsa-index.sql' );
+	run_sql_file( $db, 'analyze-wp-posts.sql' );
+	run_sql_file( $db, 'wp-posts-data.sql' );
+}
+function test3($db) {
+	run_explain_count( $db );
+}
 
-show_mysql_version( $db );
-drop_table( $db );
-run_sql_file( $db, 'create-table.sql' );
-run_sql_file( $db, 'autoinc.sql' );
-run_sql_file( $db, 'wp-posts-data.sql' );
-run_explain_count( $db, $argv );
-echo "\n";
-
-//
-
-echo "\n***** Test: 2 *****\n";
-echo "> New TSA index immediately after create table, before insert\n";
-echo "\n";
-
-show_mysql_version( $db );
-drop_table( $db );
-run_sql_file( $db, 'create-table.sql' );
-run_sql_file( $db, 'autoinc.sql' );
-run_sql_file( $db, 'tsa-index.sql' );
-run_sql_file( $db, 'wp-posts-data.sql' );
-run_explain_count( $db, $argv );
-echo "\n";
-
-//
-
-echo "\n***** Test: 2b *****\n";
-echo "> New TSA index immediately after create table, before insert (alt data set)\n";
-echo "\n";
-
-show_mysql_version( $db );
-drop_table( $db );
-run_sql_file( $db, 'create-table.sql' );
-run_sql_file( $db, 'autoinc.sql' );
-run_sql_file( $db, 'tsa-index.sql' );
-run_sql_file_each_line( $db, 'wp-posts-data-alt.sql' );
-run_explain_count( $db, $argv );
-echo "\n";
-
-//
-
-echo "\n***** Test: 3 *****\n";
-echo "> New TSA index immediately after create table, then analyze, before insert\n";
-echo "\n";
-
-show_mysql_version( $db );
-drop_table( $db );
-run_sql_file( $db, 'create-table.sql' );
-run_sql_file( $db, 'autoinc.sql' );
-run_sql_file( $db, 'tsa-index.sql' );
-run_sql_file( $db, 'analyze-wp-posts.sql' );
-run_sql_file( $db, 'wp-posts-data.sql' );
-run_explain_count( $db, $argv );
-echo "\n";
-
-//
-
-echo "\n***** Test: 4 *****\n";
-echo "> New TSA index after inserting data\n";
-echo "\n";
-
-show_mysql_version( $db );
-drop_table( $db );
-run_sql_file( $db, 'create-table.sql' );
-run_sql_file( $db, 'autoinc.sql' );
-run_sql_file( $db, 'wp-posts-data.sql' );
-run_sql_file( $db, 'tsa-index.sql' );
-run_explain_count( $db, $argv );
-echo "\n";
+function test4_setup($db) {
+	echo "\n***** Test: 4 *****\n";
+	echo "> New TSA index after inserting data\n";
+	echo "\n";
+	run_sql_file( $db, 'create-table.sql' );
+	run_sql_file( $db, 'autoinc.sql' );
+	run_sql_file( $db, 'wp-posts-data.sql' );
+	run_sql_file( $db, 'tsa-index.sql' );
+}
+function test4($db) {
+	run_explain_count( $db );
+}
 
 //
 // *****
 // Helper Functions
 // *****
 //
+
+function time_tests( array $tests ) {
+	$db = new mysqli(
+		'127.0.0.1',
+		'test_user',
+		'test_pass',
+		'test_db',
+		6330
+	);
+
+	if ( $db->connect_error ) {
+		echo "Connection error: {$db->connect_error}\n";
+		exit( 1 );
+	}
+
+	try {
+		$db->query( "SET SESSION sql_mode = REPLACE(@@sql_mode, 'NO_ZERO_DATE', '')" );
+		$db->query( "SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION'" );
+		show_mysql_version( $db );
+
+		foreach ( $tests as $setup ) {
+			$i = ITERATIONS;
+			drop_table( $db );
+			$setup( $db );
+			$start = microtime( true );
+			while ( $i-- ) {
+				run_select_count( $db );
+			}
+			$end = microtime( true );
+			$duration = $end - $start;
+			echo "Duration: " . number_format( $duration, 4 ) . " seconds\n";
+		}
+	} finally {
+		$db->close();
+	}
+}
 
 function show_mysql_version( $db ) {
 	$result = $db->query( "SELECT VERSION()" );
@@ -127,19 +142,36 @@ function run_sql_file_each_line( $db, $sql_file ) {
 	}
 }
 
-function run_explain_count( $db, $argv ) {
-	$sql = "EXPLAIN SELECT COUNT( 1 )
+const EXPLAIN_SQL = "EXPLAIN SELECT COUNT( 1 )
             FROM wp_posts
             WHERE post_type = 'post'
             AND post_status NOT IN ( 'trash','auto-draft','inherit','request-pending','request-confirmed','request-failed','request-completed' )
             AND post_author = 1";
 
-	$result = $db->query( $sql );
+const SELECT_SQL = "SELECT COUNT( 1 )
+            FROM wp_posts
+            WHERE post_type = 'post'
+            AND post_status NOT IN ( 'trash','auto-draft','inherit','request-pending','request-confirmed','request-failed','request-completed' )
+            AND post_author = 1";
+
+function run_explain_count( $db ) {
+	$result = $db->query( EXPLAIN_SQL );
 	$row = $result->fetch_assoc();
-
-	echo "Using index: {$row['key']}\n";
-
-	if ( isset( $argv[1] ) && $argv[1] === '-v' ) {
-		print_r( $row );
-	}
+	echo "Result: {$row['key']}\n";
+	print_r( $row );
 }
+
+function run_select_count( $db ) {
+	$result = $db->query( SELECT_SQL );
+	$res = $result->fetch_column(0);
+	/* print_r( $res ); */
+	/* echo "Result: {$res}\n"; */
+}
+
+time_tests([
+ 	'test1_setup',
+	'test2_setup',
+	/* 'test2b_setup', */
+	'test3_setup',
+	'test4_setup',
+]);
